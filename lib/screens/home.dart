0f236f96e85f15e3580bae2650411bb78e24a994
Diego Collares <diegocollares@brq.com>
2019-12-02 09:21:07 -0300

    crrated search bar and delegate suggestions

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertube/utils/data_search.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("lib/assets/images/youtube-logo.png"),
+        ),
+        brightness: Brightness.light, 
+        centerTitle: false,
+        elevation: 0, 
+        backgroundColor: Color(0xfff5f6fa),
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center, 
+            child: Text(
+              "0",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.star, color: Colors.black),
+            onPressed: (){
+              
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.black),
+            onPressed: () async {
+              String result = await showSearch(context: context, delegate: DataSearch());
+              print(result);
+            },
+          )
+        ],
+      ),
+      body: Container(),
     );
   }
 }
